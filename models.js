e6b4153263ddd89e1b5a23883f7ed56b9a00895b
Ben Turner <exterior@benturner.com>
2012-02-26 18:02:11 -0500

    1st run with mongodb Expecting problems with page transitions now that I'm using MongoDB calls for 1st time!

---
+++ models.js
@@ -0,0 +1,17 @@
+var Rhyme = new Schema({ 
+	body : String,
+	flowID : Number
+}); 
+
+var Flow = new Schema({
+	flowID : Number,
+	name : String,
+	topic1 : String,
+	topic2 : String,
+	compiledFlow : String,
+ 	date : Date
+}); 
+
+// add schemas to Mongoose
+mongoose.model('Flow', Flow);
+mongoose.model('Rhyme', Rhyme);
--- web.js
+++ web.js
@@ -1,48 +1,35 @@
 var express = require('express');
-var ejs = require('ejs');
+var ejs = require('ejs'); // EJS (Embedded JavaScript) https://github.com/visionmedia/ejs
 var app = express.createServer(express.logger());
 
 /*********** SERVER CONFIGURATION *****************/
 app.configure(function() {
-    
-    /*********************************************************************************
-        Configure the template engine
-        We will use EJS (Embedded JavaScript) https://github.com/visionmedia/ejs
-        
-        Using templates keeps your logic and code separate from your HTML.
-        We will render the html templates as needed by passing in the necessary data.
-    *********************************************************************************/
-
-    app.set('view engine','ejs');  // use the EJS node module
-    app.set('views',__dirname+ '/views'); // use /views as template directory
-    app.set('view options',{layout:true}); // use /views/layout.html to manage your main header/footer wrapping template
-    app.register('html',require('ejs')); //use .html files in /views
-
-    /******************************************************************
-        The /static folder will hold all css, js and image assets.
-        These files are static meaning they will not be used by
-        NodeJS directly. 
-        
-        In your html template you will reference these assets
-        as yourdomain.heroku.com/img/cats.gif or yourdomain.heroku.com/js/script.js
-    ******************************************************************/
+    app.set('view engine','ejs');
+    app.set('views',__dirname+ '/views');
+    app.set('view options',{layout:true});
+    app.register('html',require('ejs'));
     app.use(express.static(__dirname + '/static'));
-    
-    //parse any http form post
     app.use(express.bodyParser());
-    
-    /**** Turn on some debugging tools ****/
     app.use(express.logger());
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-
 });
-/*********** END SERVER CONFIGURATION *****************/
+
+var mongoose = require('mongoose');
+var schema = mongoose.Schema; 
+
+/************ DATABASE CONFIGURATION **********/
+app.db = mongoose.connect(process.env.MONGOLAB_URI); // requires .env file
+
+require('./models').configureSchema(schema, mongoose);
+
+var Flow = mongoose.model('Flow');
+var Rhyme = mongoose.model('Rhyme');
+
+
+
 
 /* Gonna have to add console.log() msgs to track the parameters,
  * namely the countdown clock. */
-
-var flowArray = []; // holds each submitted flow event
-var flowNumber, flowData;
 
 app.get('/', function(request, response) {
     var templateData = {
@@ -50,6 +37,7 @@ app.get('/', function(request, response) {
     };
     response.render("index.html", templateData);
 });
+
 
 /* I think this'll need some work. Create a counter once someone
  * starts making rhymes, but it'll need an event ID so others can
@@ -59,20 +47,64 @@ app.get('/create', function(request, response) {
     var templateData = {
         pageTitle : "Step #1: Create da Rhymes :: Karaoke Flow"
     };
+    var topics = new Array('basketball', 'fame', 'football', 'women', 'riches', 'violence', 'nyc','oakland', 'cops', 'federal govt', 'mom', 'dad', 'swag', 'tennis', 'twitter', 'skype', 'champagne', 'itp');
+
+    var randomTopic1 = function() { return Math.floor(Math.random() * topics.length); };
+    var randomTopic2 = function() { return Math.floor(Math.random() * topics.length); };
+
     response.render("create.html", templateData);
 });
 
 
 
-
-app.post('/create', function(request, response) {
-    console.log("Inside app.post('/')");
+app.post('/create/new', function(request, response) {
+    console.log("Inside app.post('/create')");
     console.log("form received and includes:")
     console.log(request.body);
-    var templateData = {
-        pageTitle : "Step #1: Create da Rhymes :: Karaoke Flow"
+
+    var flowCount = Flow.count(); // count starts at 0
+    console.log(flowCount);
+        
+    // prepare new flow with the form data
+    var flowData = {
+        flowID : flowCount,
+        name : request.body.newFlowName
     };
     
+    var newFlow = new Flow(flowData);
+    newFlow.save();
+
+    response.redirect("/create/id/" + flowID); // send to general /create page
+});
+
+
+app.post('/create', function(request, response) {
+    console.log("Inside app.post('/create')");
+    console.log("form received and includes:")
+    console.log(request.body);
+   
+    Flow.findOne({name:request.params.flowName}, function(err,flow) {
+
+        if (err) {
+            console.log('Error');
+            console.log(err);
+            response.send("Uh oh, can't find that flow!");
+        }
+        else {
+            // have to do checking for multiple rooms w/ same name
+            response.redirect("/create/id/" + flow.flowID); // send to general /create page
+        }
+    });
+});
+
+
+
+app.post('/create/id/:flowID', function(request, response) {
+    console.log("Inside app.post('/create/id')");
+    console.log("form received and includes:")
+    console.log(request.body);
+    
+    /*
     var newFlow = {
         rhyme : request.body.rhyme,
         startTime : Date.now()
@@ -81,38 +113,79 @@ app.post('/create', function(request, response) {
     
     flowArray.push(newFlow);
     flowNumber = flowArray.length - 1;
+    */
 
-    response.redirect("/perform/" + flowNumber);
+    Flow.findOne({name:request.params.flowName}, function(err,flow) {
+
+        if (err) {
+            console.log('Error');
+            console.log(err);
+            response.send("Uh oh, can't find that flow!");
+        }
+        else {
+
+var flowCount = Flow.count(); // count starts at 0
+    console.log(flowCount);
+    
+    // Prepare, save and redirect
+
+    var rhymeData = {
+        body : request.body.rhyme,
+        flowID : flowCount
+    };
+
+    //console.log(newFlow.startTime);
+
+    var newRhyme = new Rhyme(rhymeData);
+    newRhyme.save;
+        
+    // prepare new flow with the form data
+    var flowData = {
+        flowID : flowCount,
+        name : request.body.topic1,
+        text : request.body.topic2,
+        compiledFlow : request.body.rhyme
+    };
+        
+    // create new comment
+    var newFlow = new Flow(flowData);
+    
+    newFlow.save();
+
+    response.redirect("/perform/" + flowID);
+    }
+        });
+
 });
 
 
 
 
 app.get('/perform/:flowNumber', function(request, response) {
-    flowNumber = request.params.flowNumber;
-    flowData = flowArray[flowNumber];
 
     var templateData = {
         pageTitle : "Step #2: Perform da Rhymes :: Karaoke Flow"
     };
 
-    if (flowData != undefined) {
-        
-        // Render the card_display template - pass in the cardData
+    Flow.findOne({flowID:request.params.flowNumber}, function(err,flow) {
+
+        if (err) {
+            console.log('Error');
+            console.log(err);
+            response.send("Uh oh, can't find that flow!");
+        }
+
+        // Render the perform template - pass in the flowData.
         response.render("perform.html", flowData);
-        
-    } else {
-        // card not found. show the 'Card not found' template
-        response.render("notfound.html");
-        
-    }
+
+    });
 
 });
 
 
 
 
-// Make server turn on and listen at defined PORT (or port 3000 if is not defined)
+// Make server turn on and listen at defined PORT (or port 3000 if is not defined).
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
