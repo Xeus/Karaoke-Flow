1dbda4109716f3ce8bb3fb8f41d53fba6fd4efae
Ben Turner <exterior@benturner.com>
2012-02-26 21:35:22 -0500

    debugging /create/new

--- web.js
+++ web.js
@@ -132,8 +132,10 @@ app.get('/create/id/:flowID', function(request, response) {
     };
     var topics = new Array('basketball', 'fame', 'football', 'women', 'riches', 'violence', 'nyc','oakland', 'cops', 'federal govt', 'mom', 'dad', 'swag', 'tennis', 'twitter', 'skype', 'champagne', 'itp');
 
-    randomTopic1 = function() { return Math.floor(Math.random() * topics.length); };
-    randomTopic2 = function() { return Math.floor(Math.random() * topics.length); };
+    getRandomTopic = function() { return Math.floor(Math.random() * topics.length); };
+    var randomTopic1 = getRandomTopic();
+    var randomTopic2 = getRandomTopic();
+
 
     Flow.findOne({name:request.params.flowName}, function(err,flow) {
 
