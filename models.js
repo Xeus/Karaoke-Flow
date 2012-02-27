659db4e577581062e2914e39531fe3d11478b389
Ben Turner <exterior@benturner.com>
2012-02-26 20:10:04 -0500

    had to add export for model.js

--- models.js
+++ models.js
@@ -1,17 +1,19 @@
-var Rhyme = new Schema({ 
-	body : String,
-	flowID : Number
-}); 
+module.exports.configureSchema = function(Schema, mongoose) {
+	var Rhyme = new Schema({ 
+		body : String,
+		flowID : Number
+	}); 
 
-var Flow = new Schema({
-	flowID : Number,
-	name : String,
-	topic1 : String,
-	topic2 : String,
-	compiledFlow : String,
- 	date : Date
-}); 
+	var Flow = new Schema({
+		flowID : Number,
+		name : String,
+		topic1 : String,
+		topic2 : String,
+		compiledFlow : String,
+ 		date : { type: Date, default: Date.now }
+	}); 
 
-// add schemas to Mongoose
-mongoose.model('Flow', Flow);
-mongoose.model('Rhyme', Rhyme);
+	// add schemas to Mongoose
+	mongoose.model('Flow', Flow);
+	mongoose.model('Rhyme', Rhyme);	
+});
